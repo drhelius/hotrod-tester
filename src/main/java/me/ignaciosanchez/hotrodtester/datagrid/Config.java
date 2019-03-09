9cdb81866379340bd35b085f0cd08d51d40e7674
Ignacio Sanchez Gines <nacho@geardome.com>
2019-03-09 23:14:30 +0100

    near cache and protocol version

--- src/main/java/me/ignaciosanchez/hotrodtester/datagrid/Config.java
+++ src/main/java/me/ignaciosanchez/hotrodtester/datagrid/Config.java
@@ -1,7 +1,9 @@
 package me.ignaciosanchez.hotrodtester.datagrid;
 
+import org.infinispan.client.hotrod.ProtocolVersion;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.configuration.NearCacheMode;
 import org.infinispan.spring.remote.provider.SpringRemoteCacheManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
@@ -20,6 +22,15 @@ public class Config {
     @Value("${datagrid.port}")
     private String port;
 
+    @Value("${hotrod.version}")
+    private String version;
+
+    @Value("${datagrid.near-cache}")
+    private boolean nearCache;
+
+    @Value("${datagrid.near-cache-size}")
+    private int nearCacheSize;
+
     @Bean
     @Primary
     public CacheManager cacheManager() {
@@ -30,10 +41,14 @@ public class Config {
     public RemoteCacheManager infinispanCacheManager() {
 
         ConfigurationBuilder builder = new ConfigurationBuilder();
-        builder.addServer()
+        builder.version(ProtocolVersion.parseVersion(version))
+                .addServer()
                 .host(host)
                 .port(Integer.parseInt(port));
-        //builder.nearCache().mode(NearCacheMode.INVALIDATED).maxEntries(100);
+
+        if (nearCache) {
+            builder.nearCache().mode(NearCacheMode.INVALIDATED).maxEntries(nearCacheSize);
+        }
 
         return new RemoteCacheManager(builder.build());
     }
